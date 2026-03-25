abae5adcb590f0ab61b357397edf7aa1a82e3e21
Diego Fondevilla <fondevilla@chapman.edu>
2026-03-24 18:12:14 -0700

    visualized years and figuring out how to group years by era

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -19,14 +19,13 @@ listening_history.head()
 music_info['genre'].head()
 music_info['tags'].head()
 
-## Obtaining all column 
 music_info.columns
 listening_history.columns
 
 ## Visualizing genre + tag distribution
 print(music_info['tags'].isna().sum()) # Checking to see how many NA rows are there for tags 
 print(music_info['genre'].isna().sum()) # Checking to see how many NA row are there for genre 
-total_na = music_info['tags'].isna().sum() & music_info['genre'].isna().sum()
+total_na = music_info['tags'].isna() & music_info['genre'].isna()
 print(total_na.sum())
 
 ## Genre
@@ -49,6 +48,39 @@ plt.xlabel('Tag Count', fontsize = 12)
 plt.ylabel('Tag', fontsize = 12)
 print(len(tag_count.index))
 
+## Visualizing year
+print(music_info['year'].describe())
+
+music_info['era'] = music_info['year'].astype(str)
+music_info.loc[music_info['year'] < 1950, 'era'] = "Pre-1950s"
+music_info.loc[(music_info['year'] >= 1950) & (music_info['year'] < 1960), 'era'] = "1950s"
+music_info.loc[(music_info['year'] >= 1960) & (music_info['year'] < 1970), 'era'] = "1960s"
+music_info.loc[(music_info['year'] >= 1970) & (music_info['year'] < 1980), 'era'] = "1970s"
+music_info.loc[(music_info['year'] >= 1980) & (music_info['year'] < 1990), 'era'] = "1980s"
+music_info.loc[(music_info['year'] >= 1990) & (music_info['year'] < 2000), 'era'] = "1990s"
+music_info.loc[(music_info['year'] >= 2000) & (music_info['year'] < 2010), 'era'] = "2000s"
+music_info.loc[(music_info['year'] >= 2010) & (music_info['year'] < 2020), 'era'] = "2010s"
+music_info.loc[music_info['year'] >= 2020, 'era'] = "2020s"
+
+era_distribution = music_info['era'].value_counts()
+era_order = ['Pre-1950s', '1950s', '1960s', '1970s', '1980s', '1990s', '2000s', '2010s', '2020s']
+plt.figure(figsize =(12,5))
+ax = sns.barplot(x = era_distribution.index, y = era_distribution.values, order = era_order, palette = 'viridis')
+ax.bar_label(ax.containers[0], padding = 3, fontsize = 10)
+plt.title('Era distribution', fontsize = 15)
+plt.xlabel('Era', fontsize = 12)
+plt.ylabel('Era count', fontsize = 12)
+plt.ylim(0, era_distribution.max() * 1.1)
+plt.show()
+
+# year_distribution = music_info['year'].value_counts().sort_index()
+# plt.figure(figsize=(12,8))
+# sns.barplot(x = year_distribution.index, y = year_distribution.values, palette = 'viridis')
+# plt.title('Year distribution', fontsize = 15)
+# plt.xlabel('Year', fontsize = 12)
+# plt.ylabl('Year count', fontsize = 12)
+# print(year_distribution.index)
+
 
 ## Visualizing continious music data
 continuous = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms']
@@ -56,7 +88,7 @@ music_info[continuous].hist(bins=30, figsize=(12, 8), color='teal')
 plt.suptitle("Continuous Feature Distributions")
 plt.show()
 
-# Visualizing categorical music data
+## Visualizing categorical music data
 categorical = ['key', 'mode', 'time_signature']
 total = len(music_info)
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
@@ -74,7 +106,7 @@ for i, col in enumerate(categorical):
 plt.tight_layout()
 plt.show()
 
-# Visualizing the playcounts
+## Visualizing the playcounts
 fig, ax = plt.subplots(1, 1, figsize=(8, 6))
 sns.histplot(np.log1p(listening_history['playcount']), bins = 100, kde = True, ax = ax, color = 'teal')
 ax.set_title = ('Log-Playcount distribution')
