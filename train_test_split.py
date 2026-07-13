064f69499e141e44dfede7d514147066da9fbea0
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-14 00:08:42 +0800

    Checked distributions of testing set, validation set and training set to make sure the distributions are consistent

--- train_test_split.py
+++ train_test_split.py
@@ -5,12 +5,17 @@ Created on Mon Jul 13 23:00:22 2026
 @author: dbf98
 """
 # Imports
+
+## Data Manipulation
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 
-## Importing preprocessed listening history
+## Data Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Importing preprocessed listening history
 listening_history = pd.read_parquet(r'C:\Users\dbf98\Desktop\Python_Projects\Song_Recommendation\data\processed\user_history.pkl')
 listening_history.head()
 
@@ -32,3 +37,39 @@ test_set_ids, validation_set_ids = train_test_split(
 test_set = listening_history[listening_history['user_id'].isin(test_set_ids)]
 validation_set = listening_history[listening_history['user_id'].isin(validation_set_ids)]
 cv_training_set = listening_history[listening_history['user_id'].isin(cv_training_set_ids)]
+
+# Visualizing test set, validation set and training set to ensure that the distribution of playcount data is consistent throughout
+
+## Visualizing testing_set playcounts
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+sns.histplot(test_set['log_playcount'], bins = 100, kde = True, ax = ax, color = 'teal')
+ax.set_title('Test set Log-Playcount distribution')
+ax.set_xlabel('Test set Log-Playcount')
+ax.set_ylabel('Number of songs')
+plt.tight_layout()
+plt.show()
+
+## Visualizing validation_set playcounts
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+sns.histplot(validation_set['log_playcount'], bins = 100, kde = True, ax = ax, color = 'teal')
+ax.set_title('Validation set Log-Playcount distribution')
+ax.set_xlabel('Validation set Log-Playcount')
+ax.set_ylabel('Number of songs')
+plt.tight_layout()
+plt.show()
+
+## Visualizing training_set playcounts
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+sns.histplot(cv_training_set['log_playcount'], bins = 100, kde = True, ax = ax, color = 'teal')
+ax.set_title('Training set Log-Playcount distribution')
+ax.set_xlabel('Training set Log-Playcount')
+ax.set_ylabel('Number of songs')
+plt.tight_layout()
+plt.show()
+
+## Printing out log_playcount descriptions to double check 
+print(test_set['log_playcount'].describe())
+print(validation_set['log_playcount'].describe())
+print(cv_training_set['log_playcount'].describe())
+
+# Exporting sets since the distributions are acceptable
