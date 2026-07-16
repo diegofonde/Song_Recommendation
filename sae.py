b71bfffedd22e67f12710c003e93413b9eea5eff
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-16 20:34:48 +0800

    Optimized memory by switching dictionaries to sparse matrix

--- sae.py
+++ sae.py
@@ -45,36 +45,31 @@ max_train_user = training_set['user_id'].max()
 num_users = max(max_test_user, max_validation_user, max_train_user)
 print(num_users)
 
+test_set.head()
 
-# Properly convertining data into dictionary format, ready for SAE input
-def convert(data, total_tracks):
+# Properly convertining data into sparse matrix format, ready for SAE input. This is important to optimize memory that all those 0s would usually take up if I use a dictionary/array format
+def convert(data, total_tracks, total_users):
     
-    new_data = {}
+    # First step is getting the indices that represents all the users and all of the tracks + the log playcount values
+    user_indices = data['user_id'].values - 1
+    track_indices = data['track_id'].values - 1
+    log_playcount_values = data['log_playcount'].values
     
-    for user_id, group in data.groupby('user_id'):
-        
-        # Create a single, blank vector just for this specific user
-        user_vector = torch.zeros(total_tracks, dtype = torch.float32)
-        
-        # Pull out the tracks and playcounts for this user
-        track_ids = group['track_id'].values - 1
-        playcounts = group['log_playcount'].values
-        
-        # Fill just this user's vector
-        user_vector[track_ids] = torch.tensor(playcounts, dtype=torch.float32)
-        
-        # Save it to our dictionary
-        new_data[user_id] = user_vector
-        
-    return new_data
+    indices = torch.tensor(np.vstack([user_indices, track_indices]), dtype = torch.long) # Indices has user_indices on top of the stacked torch array representing the user listening to a track, and below it is the track that the user listen to
+    values = torch.tensor(log_playcount_values, dtype = torch.float32) # Separate 1D torch array that represents the log playcounts
+    
+    sparse_matrix = torch.sparse_coo_tensor(indices, values, size = (num_users, num_tracks)) # Combining it all here to make a sparse matrix, should have O(1)
+    
+    return sparse_matrix.coalesce()
+    
 
-test_set_converted = convert(test_set, num_tracks)
+test_set_converted = convert(test_set, num_tracks, num_users)
 del test_set
 gc.collect()
-validation_set_converted = convert(validation_set, num_tracks)
+validation_set_converted = convert(validation_set, num_tracks, num_users)
 del validation_set
 gc.collect()
-training_set_converted = convert(training_set, num_tracks)  
+training_set_converted = convert(training_set, num_tracks, num_users)  
 del training_set
 gc.collect()
 
@@ -117,7 +112,6 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(
     sae.parameters(), 
     lr=0.001, 
-    weight_decay=1e-5
 )
 
 nb_epoch = 50
@@ -126,11 +120,14 @@ for epoch in range(0, nb_epoch):
     train_loss = 0
     s = 0.
         
-    for id_user in training_set_converted.keys():
+    for id_user in range(num_users):
         
-        user_history = training_set_converted[id_user] # Gets the vectorized listening history of the user
+        user_vector = training_set_converted[id_user].to_dense() # Gets the vectorized listening history of the user
         
-        inputs = user_history.unsqueeze(0) # Formats data in to [1, num_tracks]
+        if torch.sum(user_vector) == 0: # If the specific split does not has any listening history for the user
+            continue
+        
+        inputs = user_vector.unsqueeze(0) # Formats data in to [1, num_tracks]
         target = inputs.clone() # Actually values to be compared post decoder
         
         if torch.sum(target.data > 0) > 0:
@@ -138,7 +135,7 @@ for epoch in range(0, nb_epoch):
             output = sae.forward(inputs)
             output = torch.where(target == 0, 0.0, output) # Makes it so that log playcount values that are actually 0 will automatically be predicted as 0, helps with model performance
             loss = criterion(output, target)
-            train_loss += loss
+            train_loss += loss.item()
             optimizer.zero_grad()
             loss.backward()
             s += 1.
