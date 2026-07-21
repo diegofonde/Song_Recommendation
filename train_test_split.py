592cea64aa19b305ae68242fd62875da0bdcac7c
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-21 22:36:09 +0800

    Testing filtered sets

--- train_test_split.py
+++ train_test_split.py
@@ -21,13 +21,19 @@ import os
 listening_history = pd.read_parquet(r'C:\Users\dbf98\Desktop\Python_Projects\Song_Recommendation\data\processed\user_history.pkl')
 listening_history.head()
 
-# Cutting down dataset to only contain the tracks with 10 or more playcounts
+# Cutting down dataset to only contain the tracks with 5 or more total log_playcounts
 listening_history_tracks = listening_history.groupby(by = 'track_id')['log_playcount'].sum().reset_index()
 listening_history_tracks.head()
+listening_history_tracks.describe()
+listening_history_tracks_top_playcount = listening_history_tracks[listening_history_tracks['log_playcount'] >= 5]
+listening_history_tracks_top_playcount.describe()
+top_playcount_track_id =  listening_history_tracks_top_playcount['track_id'].unique()
 
+# Filtering out
+listening_history_filtered = listening_history[listening_history['track_id'].isin(top_playcount_track_id)]
 
 # Getting the list of unique users from the dataset 
-unique_ids = listening_history['user_id'].unique() # Important so that in the test set we won't be predicting the tags of users that has data already inputted in the training set
+unique_ids = listening_history_filtered['user_id'].unique() # Important so that in the test set we won't be predicting the tags of users that has data already inputted in the training set
 
 cv_training_set_ids, test_set_ids = train_test_split(
     unique_ids,
@@ -41,9 +47,9 @@ test_set_ids, validation_set_ids = train_test_split(
     random_state = 42
 )
 
-test_set = listening_history[listening_history['user_id'].isin(test_set_ids)]
-validation_set = listening_history[listening_history['user_id'].isin(validation_set_ids)]
-cv_training_set = listening_history[listening_history['user_id'].isin(cv_training_set_ids)]
+test_set = listening_history_filtered[listening_history_filtered['user_id'].isin(test_set_ids)]
+validation_set = listening_history_filtered[listening_history_filtered['user_id'].isin(validation_set_ids)]
+cv_training_set = listening_history_filtered[listening_history_filtered['user_id'].isin(cv_training_set_ids)]
 
 # Visualizing test set, validation set and training set to ensure that the distribution of playcount data is consistent throughout
 
